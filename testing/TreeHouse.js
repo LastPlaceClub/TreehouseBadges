bac07ce0aecac5ec949f5177d6ad655acd77e363
Kathleen Kent <lashkath@gmail.com>
2015-09-04 11:50:50 -0700

    added testing folder

---
+++ testing/TreeHouse.js
@@ -0,0 +1,217 @@
+// mycode.js
+var usernames = ["mitchelllillie", "patharryux", "jeffdunn", "donguyen", "mkelley2", "josephfraley2", "kathleenkent", "adamtaitano", "jasonsiren", "jtz1983", "erikphansen", "tybrenner"];
+var users = [];
+var master = ['izelnakri'];
+var arrayOfAll = [];
+var object = {};
+var newObj = {};
+var inputValue;
+var arr = [
+  {val : -1, text: 'Select A User'},
+  // {val : 2, text: 'Do Nguyen'},
+  // {val : 3, text: 'Jeff Dunn'}
+];
+  var obj = {};
+
+usernames.forEach(function(name) {
+    $.get('http:/teamtreehouse.com/'+name+'.json').
+        done(importUser);
+});
+
+function importUser(user) {
+	var obj = {};
+	obj.name = (user.name);
+	obj.badges = [];
+	for(var i = 0; i < user.badges.length; i++) {
+		obj.badges.push(user.badges[i].icon_url);
+	}
+	users.push(obj);
+
+	if(users.length >= usernames.length) {
+		badgeCtor(users);
+	}
+
+}
+
+
+function loadMaster() {
+  $.ajax(
+  {
+  	url: "http://teamtreehouse.com/izelnakri.json",
+  	dataType: "json",
+  	async: false,
+  	success: function(data)
+  	{
+  		object = data;
+  	}
+  });
+  for (var i = 0; i < 322; i ++) {
+    arrayOfAll.push(object.badges[i].icon_url);
+  }
+  return arrayOfAll;
+}
+//
+// $(function(){
+//     $('button').on('click',function(){
+//         var r= $('<input type="button" value="Recommend Badges"/>');
+//         $('#TextBoxDiv').append(r);
+//     });
+// });
+//
+//
+// $(document).ready(function(){
+//
+//     var counter = 2;
+//
+//     $("#addButton").click(function () {
+//
+// 	if(counter>10){
+//             alert("Only 10 textboxes allow");
+//             return false;
+// 	}
+//
+// 	var newTextBoxDiv = $(document.createElement('div'))
+// 	     .attr("id", 'TextBoxDiv' + counter);
+//
+// 	newTextBoxDiv.after().html('<label>Textbox #'+ counter + ' : </label>' +
+// 	      '<input type="text" name="textbox' + counter +
+// 	      '" id="textbox' + counter + '" value="" >');
+//
+// 	newTextBoxDiv.appendTo("#TextBoxesGroup");
+//
+//
+// 	counter++;
+//      });
+//
+//      $("#removeButton").click(function () {
+// 	if(counter==1){
+//           alert("No more textbox to remove");
+//           return false;
+//        }
+//
+// 	counter--;
+//
+//         $("#TextBoxDiv" + counter).remove();
+//
+//      });
+//
+//      $("#getButtonValue").click(function () {
+//
+// 	var msg = '';
+// 	for(i=1; i<counter; i++){
+//    	  msg += "\n Textbox #" + i + " : " + $('#textbox' + i).val();
+// 	}
+//     	  alert(msg);
+//      });
+//   });
+//
+//   function updateNow(evt){
+//       if (evt.target.type == "button") {
+//           var div = evt.target.parentNode;
+//           var input = div.querySelector('.user_input');
+//           inputValue = input.value;
+//           div.querySelector('.output').innerHTML = inputValue;
+//       }
+//   }
+//   function addUser(inputValue) {
+//     var temp = inputValue;
+//     usernames.push(inputValue);
+//     console.log(temp);
+//     return usernames;
+//   }
+// addEventListener('click', updateNow);
+// addEventListener('click', addUser);
+
+function myFunction() {
+
+
+    var x = document.getElementById("myText").value;
+    var y = 12;
+    if (x === "") {
+      return false;
+    }
+
+    $( "div" ).remove();
+    console.log(x);
+    usernames.push(x);
+    $.ajax(
+    {
+      url: 'http://teamtreehouse.com/'+x+'.json',
+      dataType: "json",
+      async: false,
+      success: function(data)
+      {
+        var newUser = data;
+        importUser(newUser);
+
+
+      },
+      error: function(){
+        alert("Not A Real User!");
+      }
+      });
+}
+          //
+          // // var $div = $('<div>');
+          // // $div.appendTo('body');
+          //
+          //   var $h1 = $('<h1>');
+          //   $h1.appendTo($div);
+          //
+          //   var userName = users[y].name;
+          //   $h1.html(userName);
+          //
+          //   var $p = $('<p>');
+          //   $p.appendTo($h1);
+          //
+          //   for(var j = 0; j < users[y].badges.length; j++) {
+          //
+          //     var $badge_img = $('<img src="' + users[y].badges[j] + '">');
+          //     $p.append($badge_img);
+          //
+          //             y = y + 1;
+          //
+
+//}
+
+
+
+// console.log(form.elements.username.type);
+var onSubmit1 = function()
+{
+    alert('Matthew Kelley');
+};
+
+var onSubmit2 = function()
+{
+    alert('Stupid');
+};
+
+// $('.target').change(function() {
+    var dropdown = $('.target');
+    var text = $( ".target option:selected" ).text();
+    console.log(text);
+    var $button1= $('#button1');
+  $button1.unbind('click');
+  $button1.click(onSubmit2);
+ if(document.getElementById('option').value == "Matthew Kelley"){
+    $button1.click(onSubmit1);
+        //return;
+  }
+
+
+
+//});
+
+
+
+
+
+
+window.onload = function() {
+  console.log("loaded!");
+  loadMaster();
+  console.log(document.getElementById('option').value);
+
+};
+
---
+++ testing/TreeHouse_Badges.js
@@ -0,0 +1,168 @@
+var badgeCtor = function(users) {
+
+	var $div = $('<div id=something>');
+	$div.appendTo('body');
+
+	for(var i = 0; i < (users.length); i++) {
+		var $h1 = $('<h1>');
+		$h1.appendTo($div);
+
+		 obj = {val: i, text: users[i].name};
+		 arr.push(obj);
+
+
+		var userName = users[i].name;
+		$h1.html(userName);
+
+		var $p = $('<p>');
+		$p.appendTo($h1);
+
+		for(var j = 0; j < users[i].badges.length; j++) {
+
+			var $badge_img = $('<img src="' + users[i].badges[j] + '">');
+			$p.append($badge_img);
+		}
+	}
+
+	var sel = $('<select class="target">').appendTo('#awesome');
+	$(arr).each(function() {
+	 sel.append($("<option value='option1' id='option'>").attr('value',this.val).text(this.text));
+	});
+};
+
+function badgeEarnedBy(badgeURL) {
+var array = [];
+	for (var i = 0; i < users.length; i ++) {
+		for (var k = 0; k < users[i].badges.length; k ++) {
+			if (badgeURL === users[i].badges[k]) {
+				array.push(users[i].name);
+			}
+		}
+}
+return array;
+}
+
+
+function intersectBadges(usernameA, usernameB) {
+	result1 = [];
+	result2 = [];
+	for (var i = 0; i < users.length; i ++) {
+		if (usernameA === users[i].name) {
+			result1.push(users[i].badges);
+			
+		}
+	}
+	for (var k = 0; k < users.length; k ++) {
+		if (usernameB === users[k].name) {
+			result2.push(users[k].badges);
+			
+		}
+	}
+
+	result1 = _.flatten(result1)
+	result2 = _.flatten(result2)
+
+	var same = _.intersection(result1, result2);
+
+	return same
+}
+
+
+function similarity(usernameA, usernameB){
+	var one;
+	var two;
+	for (var i = 0; i < users.length; i ++) {
+		if (usernameA === users[i].name) {
+			one = Object.keys(users[i].badges).length;
+		}
+	}
+	for (var j = 0; j < users.length; j ++) {
+		if (usernameB === users[j].name) {
+			two = Object.keys(users[j].badges).length;
+		}
+	}
+    if (one > two) {
+       return ((intersectBadges(usernameA, usernameB).length) / one);
+    } else {
+        return ((intersectBadges(usernameA, usernameB).length) / two);
+    }
+}
+
+
+function score(badgeURL, username) {
+    var array = [];
+    var test = [];
+		var names = badgeEarnedBy(badgeURL);
+		for (var i = 0; i < names.length; i ++) {
+			if (username != names[i]) {
+
+			test.push(similarity(username, names[i]));
+		}
+		else {
+			console.log("poop");
+		}
+	}
+		return _.sum(test);
+}
+
+//
+function recommendBadgesFor(username) {
+	var badges = [];
+	var array = [];
+	for (var i = 0; i < users.length; i ++) {
+		if (username === users[i].name) {
+			for (var k = 0; k < users[i].badges.length; k ++) {
+		  badges.push(users[i].badges[k]);
+	 		}
+		}
+	}
+
+	var potentialBadges = _.difference(arrayOfAll, badges);
+	
+	// push into an array, the badge and the score
+	for (var j = 0; j < potentialBadges.length; j ++) {
+		array.push({badge:potentialBadges[j], score: score(potentialBadges[j], username)});
+	}
+
+	for (var m = 0; m < array.length; m++){
+		if(array[m].score !== 0) {
+			var newArray = [];
+			newArray.push(array[m])
+		}
+	}
+
+	return (newArray);
+}
+
+
+
+// badgeUrl to badgeName for multiple users
+function badgeNameMultiple(usersObject) {
+	var userBadges = [];
+	var shortURL = [];
+	var shortestURL = [];
+
+	for(var i = 0; i < users.length; i ++) {
+		var badges = users[i].badges;
+
+		for (var j = 0; j < badges.length; j++) {
+			var longURL = badges[j].split('/')
+			shortURL.push(longURL[longURL.length - 1])
+		}		
+	}
+
+	for(var k = 0; k < shortURL.length; k++) {
+		var su = (shortURL[k].split('.')[0]);
+		shortestURL.push(su)	
+	}
+
+	return shortestURL
+}
+
+// badgeURL to badgeName for single badgeURL
+function badgeNameSingle(badgeURL) {
+	var tets = badgeURL.split('/');
+	var bName = (tets[tets.length-1]);
+
+	console.log(bName.split('.')[0])
+}
